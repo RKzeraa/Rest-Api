ee8b4a7d709659563a125afb29300743b341cbb8
Ramom Karllos <rkzeraa@gmail.com>
2023-01-22 12:45:02 -0300

    [Feat] - Add has been created

--- rkdev-api/src/main/java/com/rkzeraa/api/controller/ClienteController.java
+++ rkdev-api/src/main/java/com/rkzeraa/api/controller/ClienteController.java
@@ -2,10 +2,14 @@ package com.rkzeraa.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rkzeraa.domain.model.Cliente;
@@ -33,13 +37,11 @@ public class ClienteController {
 				//.map(cliente -> ResponseEntity.ok(cliente))
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
-		
-		/*Optional<Cliente> cliente = clienteRepository.findById(clienteId);
-		
-		if(cliente.isPresent()) {
-			return ResponseEntity.ok(cliente.get());
-		}
-		
-		return ResponseEntity.notFound().build();*/
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 }
