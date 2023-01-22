0455dfb9564395b06095fa0164708df76b9f649c
Ramom Karllos <rkzeraa@gmail.com>
2023-01-22 12:20:12 -0300

    [Feat] - Search by ID

--- rkdev-api/src/main/java/com/rkzeraa/api/controller/ClienteController.java
+++ rkdev-api/src/main/java/com/rkzeraa/api/controller/ClienteController.java
@@ -2,7 +2,9 @@ package com.rkzeraa.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rkzeraa.domain.model.Cliente;
@@ -19,5 +21,23 @@ public class ClienteController {
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		
+		
+		return clienteRepository.findById(clienteId)
+				//.map(cliente -> ResponseEntity.ok(cliente))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		
+		/*Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();*/
 	}
 }
