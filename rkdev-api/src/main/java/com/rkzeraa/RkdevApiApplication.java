d5ef6ddb085ea8752f91a6edf21b5e9f070a17a2
Ramom Karllos <rkzeraa@gmail.com>
2023-01-22 12:03:19 -0300

    [Feat] - Project Api in Spring

---
+++ rkdev-api/src/main/java/com/rkzeraa/RkdevApiApplication.java
@@ -0,0 +1,13 @@
+package com.rkzeraa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkdevApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkdevApiApplication.class, args);
+	}
+
+}
---
+++ rkdev-api/src/test/java/com/rkzeraa/RkdevApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkzeraa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RkdevApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
