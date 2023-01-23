3ca10d6eb33830e51a5e2d63a6f2b1d720ba809b
Ramom Karllos <rkzeraa@gmail.com>
2023-01-22 22:03:28 -0300

    [Refactor] - Isolating Domain Model from Representation Model

---
+++ rkdev-api/src/main/java/com/rkzeraa/api/model/EntregaResponse.java
@@ -0,0 +1,23 @@
+package com.rkzeraa.api.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import com.rkzeraa.domain.model.StatusEntrega;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EntregaResponse {
+	
+	private Long id;
+	private String nomeCliente;
+	private DestinatarioResponse destinatario;
+	private BigDecimal taxa;
+	private StatusEntrega status;
+	private OffsetDateTime dataPedido;
+	private OffsetDateTime dataFinalizacao;
+	
+}
